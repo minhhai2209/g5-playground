2ae5ac0b3f26ff875bebd9ff2752d8c47a726dac
minhhai2209 <nguyenminhhai2209@gmail.com>
2022-05-30 17:27:02 +0700

    Update goto.js

--- goto.js
+++ goto.js
@@ -1,9 +1,7 @@
 const [page] = await browser.pages();
 
-await page.goto('https://news.ycombinator.com', {
-  waitUntil: 'networkidle2',
-});
-
-await page.pdf({ path: 'hn.pdf', format: 'a4' });
+await page.goto('https://duckduckgo.com/', { waitUntil: 'networkidle2' })
+await page.type('#search_form_input_homepage', 'Puppeteer')
+const searchValue = await page.$eval('#search_form_input_homepage', el => el.value)
     
 await page.close();
