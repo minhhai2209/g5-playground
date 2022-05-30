f7fd191a99e37932f0db80c066b6365427f7be81
minhhai2209 <nguyenminhhai2209@gmail.com>
2022-05-30 23:01:55 +0700

    update

--- mock-response.js
+++ mock-response.js
@@ -4,13 +4,24 @@ await page.setRequestInterception(true)
 
 page.on('request', (request) => {
     if (request.url() === 'https://danube-webshop.herokuapp.com/api/books') {
+        const mockResponseObject = [
+            {
+                id: 1,
+                title: 'How to Mock a Response',
+                author: 'A. Friend',
+                genre: 'business',
+                price: '0.00',
+                rating: '★★★★★',
+                stock: 65535
+            }
+        ]
         request.respond({
-        content: 'application/json',
-        body: JSON.stringify(mockResponseObject)
+            content: 'application/json',
+            body: JSON.stringify(mockResponseObject)
         })
     } else request.continue()
 })
 
 await page.goto('https://danube-webshop.herokuapp.com/')
-    
+
 await page.close();
